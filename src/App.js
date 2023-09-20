e68ff4c58da2bf0aefca50f60a792025600b7f26
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-09-20 17:05:46 +0530

    starting a new quiz

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./main";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Loader from "./Loader";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
@@ -17,8 +18,11 @@ const reducer = function (state, action) {
     case "dataRecieved": {
       return { ...state, questions: action.payload, status: 'ready' }
     }
-    case "error": {
+    case "dataFailed": {
       return { ...state, status: 'error' }
+    }
+    case "start": {
+      return { ...state, status: 'active' }
     }
 
     default: throw new Error("unknown action")
@@ -35,7 +39,7 @@ function App() {
     fetch('http://localhost:9000/questions')
       .then(res => res.json())
       .then(data => dispatch({ type: "dataRecieved", payload: data }))
-      .catch(err => dispatch({ type: 'error' }))
+      .catch(err => dispatch({ type: 'dataFailed' }))
 
   }, [])
 
@@ -45,8 +49,12 @@ function App() {
 
       <Main>
         {status === "loading" && <Loader />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
-        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen
+          numQuestions={numQuestions}
+          dispatch={dispatch}
+        />}
+        {status === "dataFailed" && <Error />}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
