527b545dc5dcb244f678c3f34c1b337e790bcc10
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-09-20 13:14:58 +0530

    started with fake api created using json-server npm package

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./main";
 
+const initialState = {
+  questions: [],
+
+  // loading, error, ready, active, finished
+  status: 'loading'
+};
+
+const reducer = function (state, action) {
+  switch (action.type) {
+    case "dataRecieved": {
+      return { ...state, questions: action.payload, status: 'ready' }
+    }
+    case "error": {
+      return { ...state, questions: [], status: 'error' }
+    }
+
+    default: throw new Error("unknown action")
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { questions, status } = state;
+
+  useEffect(function () {
+
+    fetch('http://localhost:9000/questions')
+      .then(res => res.json())
+      .then(data => dispatch({ type: "dataRecieved", payload: data }))
+      .catch(err => dispatch({ type: 'error' }))
+
+  }, [])
+
   return (
     <div className="app">
       <Header />
